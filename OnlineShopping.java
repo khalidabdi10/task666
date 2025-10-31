bb2158a1dfc3e13b29614de86b375e984997652a
khalidabdi10 <khalidabdirashid7000@gmail.com>
2025-10-31 18:25:01 +0300

    first commit

---
+++ OnlineShopping.java
@@ -0,0 +1,46 @@
+class Product {
+    public void purchase() {
+        System.out.println("Purchasing a generic product.");
+    }
+
+    public void displayDetails() {
+        System.out.println("Product details: Basic product information.");
+    }
+
+    public void displayDetails(String info) {
+        System.out.println("Product details: " + info);
+    }
+}
+
+class Book extends Product {
+    @Override
+    public void purchase() {
+        System.out.println("Purchasing the book: 'Effective Java'.");
+    }
+}
+
+class Electronics extends Product {
+    @Override
+    public void purchase() {
+        System.out.println("Purchasing electronic gadget: Smartphone.");
+    }
+}
+
+public class OnlineShopping {
+    public static void buyProduct(Product product) {
+        product.purchase();
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book();
+        Electronics gadget = new Electronics();
+        book.purchase();
+        gadget.purchase();
+        book.displayDetails();
+        book.displayDetails("Hardcover edition, 300 pages");
+        gadget.displayDetails();
+        gadget.displayDetails("Latest model with 5G");
+        buyProduct(book);
+        buyProduct(gadget);
+    }
+}
